4560231ae62d55500d4acbd1366c26c812ae9917
Plumelet <qwefg1059@gmail.com>
2026-05-10 14:42:25 +0800

    fix: class Base is now abstract

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -14,12 +14,8 @@ engine = create_async_engine(
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False) 
 
 class Base(AsyncAttrs, DeclarativeBase):
-  pass
-  # __abstract__ = True
+  __abstract__ = True
   
-  # @declared_attr.directive
-  # def __tablename__(cls) -> str:
-  #   return f"{cls.__name__.lower()}s"
 
 async def get_db_session():
   async with async_session_maker() as session:
