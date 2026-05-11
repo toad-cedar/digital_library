9571b8accfe0b284a9f3355dfd3e35c6eb411656
Plumelet <qwefg1059@gmail.com>
2026-05-11 23:09:30 +0800

    feat(alembic): configure env.py script and alembic.ini file

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,29 +1,25 @@
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+import os
+import sys
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+from app.config.settings import Settings
+settings = Settings()
+db_url = settings.get_sync_db_url.replace('%', '%%')
+# Переопределение URL подключения
+config.set_main_option("sqlalchemy.url", db_url)
+
 if config.config_file_name is not None:
   fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+from app.config.database import Base
+target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
@@ -45,10 +41,8 @@ def run_migrations_offline() -> None:
     literal_binds=True,
     dialect_opts={"paramstyle": "named"},
   )
-
   with context.begin_transaction():
     context.run_migrations()
-
 
 def run_migrations_online() -> None:
   """Run migrations in 'online' mode.
@@ -62,15 +56,15 @@ def run_migrations_online() -> None:
     prefix="sqlalchemy.",
     poolclass=pool.NullPool,
   )
-
   with connectable.connect() as connection:
     context.configure(
-      connection=connection, target_metadata=target_metadata
+      connection=connection, 
+      target_metadata=target_metadata,
+      compare_type=True,
+      compare_server_default=True
     )
-
     with context.begin_transaction():
       context.run_migrations()
-
 
 if context.is_offline_mode():
   run_migrations_offline()
