2da0a9dc72d381e9e7e4a4bb91e70f506d84dd76
Plumelet <qwefg1059@gmail.com>
2026-05-12 14:48:08 +0800

    feat(config): add naming conventions to Base class

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -15,8 +15,20 @@ engine = create_async_engine(
 # Создание сессии 
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False) 
 
+
+vmetadata = MetaData(
+  naming_convention={
+    "ix": "ix_%(table_name)s_%(column_0_name)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+  }
+)
+
 class Base(AsyncAttrs, DeclarativeBase):
   __abstract__ = True
+  metadata = vmetadata
 
 
 async def get_db_session():
